1689f07b2175ca981c3aee35b189e115e9ec9f36
heewoong-ahn <dksgmldnd@korea.ac.kr>
2024-08-14 01:06:57 +0900

    feature: /collections 스웨거 응답 포맷 변경.

--- src/collection/collection.controller.ts
+++ src/collection/collection.controller.ts
@@ -6,29 +6,52 @@ import {
   ParseArrayPipe,
   Post,
   Query,
+  Req,
 } from '@nestjs/common';
 import { CollectionService } from './collection.service';
 import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CreateCollectionReqDto } from './dtos/create-collection-req.dto';
 import { CrawlingCollectionReqDto } from './dtos/crawling-collection-req.dto';
+import { CustomAuthSwaggerDecorator } from 'src/common/decorators/auth-swagger.decorator';
+import { CollectionsListResDto } from './dtos/paginated-collections-list-res.dto';
+import { CollectionPlacesListResDto } from './dtos/collection-places-list-res.dto';
 
 @ApiTags('게시글 API')
 @Controller('collections')
 export class CollectionController {
   constructor(private readonly collectionService: CollectionService) {}
 
-  @ApiOperation({ summary: '조회하지 않은 게시글 조회' })
+  @CustomAuthSwaggerDecorator({
+    summary: '조회하지 않은 게시글 조회',
+    status: 200,
+    description: '조회하지 않은 게시글 조회 성공',
+    type: CollectionsListResDto,
+  })
   @ApiQuery({ name: 'cursorId', required: false })
   @Get('unviewed')
-  async getUnviewedCollections(@Query('cursorId') cursorId?: number) {
-    return await this.collectionService.getUnviewedCollections(1, cursorId);
+  async getUnviewedCollections(
+    @Req() req,
+    @Query('cursorId') cursorId?: number,
+  ) {
+    return await this.collectionService.getUnviewedCollections(
+      req.user.id,
+      cursorId,
+    );
   }
 
-  @ApiOperation({ summary: '조회한 게시글 조회' })
+  @CustomAuthSwaggerDecorator({
+    summary: '조회한 게시글 조회',
+    status: 200,
+    description: '조회한 게시글 조회 성공',
+    type: CollectionsListResDto,
+  })
   @ApiQuery({ name: 'cursorId', required: false })
   @Get('viewed')
-  async getViewedCollection(@Query('cursorId') cursorId?: number) {
-    return await this.collectionService.getViewedCollections(1, cursorId);
+  async getViewedCollection(@Req() req, @Query('cursorId') cursorId?: number) {
+    return await this.collectionService.getViewedCollections(
+      req.user.id,
+      cursorId,
+    );
   }
 
   // @Get(':collectionId') //한 가지 collection의 정보만을 얻어오기
@@ -36,10 +59,21 @@ export class CollectionController {
   //   return await this.collectionService.getCollection(collectionId);
   // }
 
-  @ApiOperation({ summary: '특정 게시글의 장소 후보 리스트 조회' })
+  @CustomAuthSwaggerDecorator({
+    summary: '특정 게시글의 장소 후보 리스트 조회',
+    status: 200,
+    description: '특정 게시글의 장소 후보 리스트 조회 성공',
+    type: CollectionPlacesListResDto,
+  })
   @Get(':collectionId/collection-places')
-  async getCollectionPlaces(@Param('collectionId') collectionId: number) {
-    return await this.collectionService.getCollectionPlaces(1, collectionId);
+  async getCollectionPlaces(
+    @Req() req,
+    @Param('collectionId') collectionId: number,
+  ) {
+    return await this.collectionService.getCollectionPlaces(
+      req.user.id,
+      collectionId,
+    );
   }
 
   @ApiOperation({ summary: '크롤링을 위해 링크 전송' })
