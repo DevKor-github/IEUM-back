8a5fb14e46732656547066756270f29a2438a1f4
Jeonghwan Park <pjhisgod@naver.com>
2024-08-27 14:14:22 +0900

    feat : firebaseService init - firebaseService 종속성 설치, 모듈 Init

--- src/crawling/crawling.controller.ts
+++ src/crawling/crawling.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { CrawlingService } from './crawling.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CrawlingCollectionReqDto } from './dtos/crawling-collection-req.dto';
 import { CustomAuthSwaggerDecorator } from 'src/common/decorators/auth-swagger.decorator';
+import { FirebaseService } from './firebase.service';
 
 @ApiTags('크롤링 API')
 @Controller('crawling')
 export class CrawlingController {
-  constructor(private readonly crawlingSerivce: CrawlingService) {}
+  constructor(
+    private readonly crawlingSerivce: CrawlingService,
+    private readonly firebaseService: FirebaseService,
+  ) {}
 
   @CustomAuthSwaggerDecorator({
     summary: '크롤링을 위해 링크 전송',
@@ -18,5 +22,10 @@ export class CrawlingController {
   @Post('')
   async requestCrawling(@Req() req, @Body() body: CrawlingCollectionReqDto) {
     return await this.crawlingSerivce.requestCrawling(req.user.id, body);
+  }
+
+  @Get('firebase')
+  async getFirebaseApp() {
+    return await this.firebaseService.getFirebaseApp();
   }
 }
---
+++ src/crawling/firebase.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+
+@Injectable()
+export class FirebaseService implements OnModuleInit {
+  private firebaseApp: admin.app.App;
+
+  onModuleInit() {
+    this.firebaseApp = admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    });
+  }
+
+  getFirebaseApp(): admin.app.App {
+    return this.firebaseApp;
+  }
+}
