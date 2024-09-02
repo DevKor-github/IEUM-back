3c9b0a6a4764a52206cde533e9e78e72a9ba50a7
Jeonghwan Park <pjhisgod@naver.com>
2024-09-02 14:48:37 +0900

    feat : IeumExceptions 정의

--- src/common/utils/exception.util.ts
+++ src/common/utils/exception.util.ts
@@ -1,15 +1,100 @@
-interface ieumExceptionData {
+interface ieumExceptionRes {
   name: string;
   message: string;
   errorCode: number;
   statusCode: number;
 }
 
-export const ieumExceptions: Record<string, ieumExceptionData> = {};
+export const ieumExceptions: Record<string, ieumExceptionRes> = {
+  NOT_VALID_REFRESH: {
+    name: 'NotValidRefresh',
+    message: 'Not Valid Refresh',
+    errorCode: 1000,
+    statusCode: 400,
+  },
+  NOT_VALID_USER: {
+    name: 'NotValidUser',
+    message: 'Not Valid User',
+    errorCode: 2001,
+    statusCode: 400,
+  },
+  NOT_VALID_COLLECTION: {
+    name: 'NotValidCollection',
+    message: 'Not Valid Collection',
+    errorCode: 3001,
+    statusCode: 400,
+  },
+  CONFLICTED_COLLECTION: {
+    name: 'ConflictedCollection',
+    message: 'Conflicted Collection',
+    errorCode: 3002,
+    statusCode: 409,
+  },
+  NOT_VALID_FOLDER: {
+    name: 'NotValidFolder',
+    message: 'Not Valid Folder',
+    errorCode: 4001,
+    statusCode: 400,
+  },
+  FORBIDDEN_FOLDER: {
+    name: 'ForbiddenFolder',
+    message: 'Forbidden Folder',
+    errorCode: 4002,
+    statusCode: 403,
+  },
+  NOT_VALID_PLACE: {
+    name: 'NotValidPlace',
+    message: 'Not Valid Place',
+    errorCode: 5001,
+    statusCode: 400,
+  },
+  BAD_REQUEST_IMAGE_FILE: {
+    name: 'BadRequestImageFile',
+    message: 'Bad Request Image File',
+    errorCode: 5002,
+    statusCode: 400,
+  },
+  AWS_S3_ERROR: {
+    name: 'AWSS3Error',
+    message: 'AWS S3 Error',
+    errorCode: 7000,
+    statusCode: 500,
+  },
+  DEFAULT_INTERNAL_SERVER_ERROR: {
+    name: 'DefaultInternalServerError',
+    message: 'Default Internal Server Error',
+    errorCode: 8000,
+    statusCode: 500,
+  },
+  DEFAULT_BAD_REQUEST: {
+    name: 'DefaultBadRequest',
+    message: 'Default Bad Request',
+    errorCode: 9000,
+    statusCode: 400,
+  },
+  DEFAULT_UNAUTHORIZED: {
+    name: 'DefaultUnauthorized',
+    message: 'Default Unauthorized',
+    errorCode: 9001,
+    statusCode: 401,
+  },
+  DEFAULT_FORBIDDEN: {
+    name: 'DefaultForbidden',
+    message: 'Default Forbidden',
+    errorCode: 9003,
+    statusCode: 403,
+  },
+  DEFAULT_UNDEFINED: {
+    name: 'DefaultUndefined',
+    message: 'Default Undefined',
+    errorCode: 9999,
+    statusCode: 500,
+  },
+};
 
 export type ieumExceptionName = keyof typeof ieumExceptions;
 
-export class ieumException extends Error {
+export class IeumException extends Error {
   errorCode: number;
   statusCode: number;
 
@@ -22,7 +107,7 @@ export class ieumException extends Error {
 }
 
 export function throwIeumException(name: ieumExceptionName): never {
-  throw new ieumException(name);
+  throw new IeumException(name);
 }
 
 //필터에서는 이러한 것들을 Catch
