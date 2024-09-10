79d1aee8b9cad839ce189f9ee425df26c9a38481
Jeonghwan Park <pjhisgod@naver.com>
2024-09-10 13:55:16 +0900

    feat : placeDetail Entity 변경

--- src/common/interfaces/google-places-api-detail.interface.ts
+++ src/common/interfaces/google-places-api-detail.interface.ts
@@ -1,10 +1,20 @@
 export interface GooglePlacesApiDetail {
-  weekDaysOpeningHours: string[];
-  parkingOptions: any;
-  allowsDogs: boolean;
-  goodForGroups: boolean;
-  takeout: boolean;
-  delivery: boolean;
-  reservable: boolean;
-  googleMapsUri: string;
+  weekDaysOpeningHours?: string[]; // 월요일 ~ 화요일 ~...
+  freeParkingLot?: boolean;
+  paidParkingLot?: boolean;
+  freeStreetParking?: boolean;
+  allowsDogs?: boolean;
+  goodForGroups?: boolean;
+  takeout?: boolean;
+  delivery?: boolean;
+  reservable?: boolean;
+  googleMapsUri?: string;
 }
+
+/*
+"parkingOptions": {
+    "freeParkingLot": false
+  }
+  parkingOptions: { freeParkingLot: false, freeStreetParking: false }
+  paidParkingLot: true
+*/
