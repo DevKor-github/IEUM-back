4248835e0debadabe5ed4bb3a88e0aefe437f86f
Jeonghwan Park <pjhisgod@naver.com>
2024-09-13 09:47:29 +0900

    feat : preference 관련 Enum 작성

---
+++ src/common/enums/preference.enum.ts
@@ -0,0 +1,28 @@
+export enum preferredRegions {
+  SEOUL = '서울',
+  GYEONGGI = '경기',
+  INCHEON = '인천',
+  BUSAN = '부산',
+  DAEGU = '대구',
+  GYEONGBUK = '경북',
+  ULSAN = '울산',
+  GYEONGNAM = '경남',
+  DAEJEON = '대전',
+  CHUNGBUK = '충북',
+  CHUNGNAM = '충남',
+  SEJONG = '세종',
+  GWANGJU = '광주',
+  JEONBUK = '전북',
+  JEONNAM = '전남',
+  JEJU = '제주',
+}
+
+export enum preferredCompanions {
+  ALONE = '나홀로',
+  FAMILY = '가족',
+  PARENTS = '부모님',
+  KIDS = '아이',
+  COUPLE = '연인',
+  FRIENDS = '친구',
+  PETS = '반려동물',
+}
