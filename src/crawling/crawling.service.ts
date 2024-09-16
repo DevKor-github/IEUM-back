f10ad1c40635e04b3e8a9b45c72a3d286f2fd890
Jeonghwan Park <pjhisgod@naver.com>
2024-09-16 21:58:48 +0900

    hotfix : CrawlingService에 Injectable 빼먹음

--- src/crawling/crawling.service.ts
+++ src/crawling/crawling.service.ts
@@ -1,12 +1,14 @@
-import { Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CollectionService } from 'src/collection/collection.service';
 import { CreateCollectionReqDto } from 'src/collection/dtos/create-collection-req.dto';
 import { throwIeumException } from 'src/common/utils/exception.util';
 import { PlaceService } from 'src/place/place.service';
 import { Transactional } from 'typeorm-transactional';
 
+@Injectable()
 export class CrawlingService {
   private readonly logger = new Logger();
+
   constructor(
     private readonly collectionService: CollectionService,
     private readonly placeService: PlaceService,
@@ -15,6 +17,7 @@ export class CrawlingService {
   @Transactional() //Transactional의 정상적인 작동 의심.
   async createCollection(createCollectionReq: CreateCollectionReqDto) {
     try {
+      console.log('createCollection at crawlingService', createCollectionReq);
       const collection =
         await this.collectionService.createCollectionWithDuplicationCheck(
           createCollectionReq,
