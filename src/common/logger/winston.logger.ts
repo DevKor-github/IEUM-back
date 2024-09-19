faebe227b67c829719133d8cdfd93645976165f9
heewoong-ahn <dksgmldnd@korea.ac.kr>
2024-09-20 03:24:43 +0900

    fix: 로그 저장 경로 수정.

--- src/common/logger/winston.logger.ts
+++ src/common/logger/winston.logger.ts
@@ -2,9 +2,9 @@ import { utilities, WinstonModule } from 'nest-winston';
 import * as winstonDaily from 'winston-daily-rotate-file';
 import * as winston from 'winston';
 
-const logDir = __dirname + '/../../logs'; // 로그 파일 저장 경로
-// const logDir = '/app/logs';
-console.log(__dirname);
+// const logDir = __dirname + '/../../logs'; // 로그 파일 저장 경로
+const logDir = '/app/logs';
+// console.log(__dirname);
 
 const transports: winston.transport[] = [
   // 모든 레벨 로그를 파일에 저장
