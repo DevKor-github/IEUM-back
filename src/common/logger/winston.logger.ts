7b388159a91db7915f6e3d8a6548c5b633e284f8
Jeonghwan Park <pjhisgod@naver.com>
2024-09-24 15:09:45 +0900

    hotfix : winston 설정 production/development/local로 분리

--- src/common/logger/winston.logger.ts
+++ src/common/logger/winston.logger.ts
@@ -2,28 +2,17 @@ import { utilities, WinstonModule } from 'nest-winston';
 import * as winstonDaily from 'winston-daily-rotate-file';
 import * as winston from 'winston';
 import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
 
 dotenv.config();
-
 // const logDir = __dirname + '/../../log s'; // 로그 파일 저장 경로
-const isProd = process.env.NODE_ENV === 'production';
-
+const nodeMode = process.env.NODE_ENV ?? 'local';
+console.log('nodeMode:', nodeMode);
 const logDir = '/app/logs';
 let transports: winston.transport[] = [];
 
-if (isProd) {
+if (nodeMode !== 'local') {
   transports = [
-    new winstonDaily({
-      level: 'silly', // silly 레벨 이상 로그를 기록
-      dirname: `${logDir}/silly`, // 로그 파일 저장 경로
-      filename: '%DATE%.silly.log', // 파일 이름 형식
-      datePattern: 'YYYY-MM-DD', // 일별로 파일 생성
-      zippedArchive: true, // 로그 파일을 압축
-      maxFiles: '30d', // 30일치 로그 파일만 보관
-      format: winston.format.json(),
-      //maxSize //단일 로그 파일의 최대 크기, 크기를 넘어서면 다른 로그 파일이 생성됨.
-    }),
-
     // error 레벨 로그를 별도로 저장
     new winstonDaily({
       level: 'error',
@@ -34,13 +23,25 @@ if (isProd) {
       maxFiles: '30d',
       format: winston.format.json(),
     }),
+    new winstonDaily({
+      level: nodeMode === 'production' ? 'info' : 'silly', // silly 레벨 이상 로그를 기록
+      dirname: nodeMode === 'production' ? `${logDir}/info` : `${logDir}/silly`, // 로그 파일 저장 경로
+      filename:
+        nodeMode === 'production' ? `%DATE%.info.log` : '%DATE%.silly.log', // 파일 이름 형식
+      datePattern: 'YYYY-MM-DD', // 일별로 파일 생성
+      zippedArchive: true, // 로그 파일을 압축
+      maxFiles: '30d', // 30일치 로그 파일만 보관
+      format: winston.format.json(),
+      //maxSize //단일 로그 파일의 최대 크기, 크기를 넘어서면 다른 로그 파일이 생성됨.
+    }),
   ];
   // 모든 레벨 로그를 파일에 저장
 }
 
+//Default - Console 표기
 transports.push(
   new winston.transports.Console({
-    level: isProd ? 'info' : 'silly', // 개발 단계에서는 모든 레벨의 로그를 출력
+    level: nodeMode === 'production' ? 'info' : 'silly', // 개발 단계에서는 모든 레벨의 로그를 출력
     format: winston.format.combine(
       winston.format.timestamp({
         format: 'YYYY-MM-DD HH:mm:ss', // 타임스탬프 형식
