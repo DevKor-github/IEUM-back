6742d1dbddf358598bb92c6d3cb2903988ae9fc8
Jeonghwan Park <pjhisgod@naver.com>
2024-10-14 14:46:15 +0900

    feat : getCollectionPlaceDetail skeleton

--- src/collection-complex/collection-complex.service.ts
+++ src/collection-complex/collection-complex.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CollectionService } from 'src/collection/collection.service';
+import { FolderService } from 'src/folder/folder.service';
+import { PlaceService } from 'src/place/services/place.service';
 
 @Injectable()
-export class CollectionComplexService {}
+export class CollectionComplexService {
+  constructor(
+    private readonly collectionService: CollectionService,
+    private readonly folderService: FolderService,
+    private readonly placeService: PlaceService,
+  ) {}
+
+  async getCollectionPlaceDetail(collectionPlaceId: number) {}
+}
