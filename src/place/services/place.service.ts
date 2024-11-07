c44b244825492568b3843341c7970351c623d148
heewoong_ahn <102745492+heewoong-ahn@users.noreply.github.com>
2024-11-08 01:52:32 +0900

    HOTFIX: call kakao api and update db with await

--- src/place/services/place.service.ts
+++ src/place/services/place.service.ts
@@ -105,7 +105,7 @@ export class PlaceService {
     });
 
     for await (const line of rl) {
-      this.createPlaceByKakaoLocal(line);
+      await this.createPlaceByKakaoLocal(line);
       // console.log(line);
     }
   }
