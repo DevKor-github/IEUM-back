8ac8a8a844d9accecd444659de5c2f7c1c501226
Jeonghwan Park <pjhisgod@naver.com>
2024-12-14 13:35:02 +0900

    docs: controller API Docs 반영

--- src/collection-complex/collection-complex.controller.ts
+++ src/collection-complex/collection-complex.controller.ts
@@ -2,8 +2,15 @@ import { Controller, Get, Param, Query, Req } from '@nestjs/common';
 import { CollectionPlacesListResDto } from 'src/collection/dtos/collection-places-list-res.dto';
 import { CollectionsListResDto } from 'src/collection/dtos/paginated-collections-list-res.dto';
 import { CollectionComplexService } from './collection-complex.service';
+import { CollectionComplexDocs } from './collection-complex.docs';
+import { ApplyDocs } from 'src/common/decorators/apply-docs.decorator';
+import { ApiTags } from '@nestjs/swagger';
+import { UseNicknameCheckingAccessGuard } from 'src/auth/guards/nickname-check-access.guard';
 
-@Controller('collection-complex')
+@UseNicknameCheckingAccessGuard()
+@ApplyDocs(CollectionComplexDocs)
+@ApiTags('게시글 API')
+@Controller('collections')
 export class CollectionComplexController {
   constructor(
     private readonly collectionComplexService: CollectionComplexService,
