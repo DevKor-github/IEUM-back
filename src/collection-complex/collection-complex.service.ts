b7d037fd80356fdf19fd3f55d671bccc837d0193
Jeonghwan Park <pjhisgod@naver.com>
2024-12-14 14:12:47 +0900

    fix: 병렬 비동기 작업 프로미스 해소

--- src/collection-complex/collection-complex.service.ts
+++ src/collection-complex/collection-complex.service.ts
@@ -42,13 +42,15 @@ export class CollectionComplexService {
       id,
       cursorId,
     );
-    viewedCollections.items.map(async (collection: CollectionDto) => {
-      const collectionPlaces: CollectionPlacesListResDto =
-        await this.getCollectionPlaces(id, collection.id);
-      collection.savedCollectionPlacesCount = collectionPlaces.items.filter(
-        (place) => place.isSaved,
-      ).length;
-    });
+    await Promise.all(
+      viewedCollections.items.map(async (collection: CollectionDto) => {
+        const collectionPlaces: CollectionPlacesListResDto =
+          await this.getCollectionPlaces(id, collection.id);
+        collection.savedCollectionPlacesCount = collectionPlaces.items.filter(
+          (place) => place.isSaved,
+        ).length;
+      }),
+    );
     return viewedCollections;
   }
 }
